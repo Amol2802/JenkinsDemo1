01c9c0de2555c37f45bf73ba9e8928306cff911e
JAYA VYAWHARE <JAYA VYAWHARE@DESKTOP-6G9M66L>
2024-07-04 09:21:31 +0530

    My first commite

---
+++ JenkinsDemo/src/main/java/com/selenium/TestDemo2.java
@@ -0,0 +1,17 @@
+package com.selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestDemo2 {
+@Test
+	public void setup() {
+
+		WebDriver driver;
+		driver = new ChromeDriver();
+		driver.get("https://omayo.blogspot.com/");
+		driver.manage().window().maximize();
+
+	}
+}
