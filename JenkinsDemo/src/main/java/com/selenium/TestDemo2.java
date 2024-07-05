f3bc1c07a77f84ab8cd607afb55db3c1e2f565c3
JAYA VYAWHARE <JAYA VYAWHARE@DESKTOP-6G9M66L>
2024-07-05 09:48:51 +0530

    ************ added new classes ***********

--- JenkinsDemo/src/main/java/com/selenium/TestDemo2.java
+++ JenkinsDemo/src/main/java/com/selenium/TestDemo2.java
@@ -2,18 +2,33 @@ package com.selenium;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 
 public class TestDemo2 {
-@Test
+	WebDriver driver;
+@BeforeMethod
 	public void setup() {
 
-		WebDriver driver;
+	if(System.getProperty("browser").equalsIgnoreCase("chrome")) {
 		driver = new ChromeDriver();
+	
+	}
+	else if(System.getProperty("browser").equalsIgnoreCase("fireFox")) {
+	
+	driver=new FirefoxDriver();
+	}
+	
 		driver.get("https://omayo.blogspot.com/");
 		driver.manage().window().maximize();
 		
-		System.out.println("*******************  This is new  statement ********************");
 
 	}
+@AfterMethod
+public void closeBrowser() {
+	
+	driver.quit();
+}
 }
